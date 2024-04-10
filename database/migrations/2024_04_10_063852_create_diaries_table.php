7f57054120b1380b7bab63c72764657cbac273f4
NaotoMakimoto <makimaki2510@gmail.com>
2024-04-10 16:05:43 +0900

    Initial commit

---
+++ database/migrations/2024_04_10_063852_create_diaries_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diaries', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('users');
+            $table->date('date');
+            $table->text('diary_content');
+            $table->integer('mood');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diaries');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="top_content">
+        <div>4/10</div>
+        <div>
+            <div>宿題</div>
+            <div>持ち物</div>
+        </div>
+    </div>
+    <div class="middle_content">
+        <div>
+
+        </div>
+    </div>
+    <div class="bottom_content">
+        <div>日記</div>
+        <div>〇</div>
+        <div><p>aaaaaaaaaaaaa</p></div>
+        <div>コメント</div>
+    </div>
+</body>
+</html>
