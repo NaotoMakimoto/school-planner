d8856869d8dcb595a334f330fde231930a35a555
NaotoMakimoto <makimaki2510@gmail.com>
2024-04-20 11:49:11 +0900

    いったん写真カレンダーに入れられるようになった

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,20 +1,32 @@
 // calendar.js
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
-// import '@fullcalendar/core/main.css';
-// import '@fullcalendar/daygrid/main.css';
 
-// Make sure the DOM is fully loaded before querying for the calendar element
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-  if (calendarEl) {
-    var calendar = new Calendar(calendarEl, {
-      plugins: [dayGridPlugin],
-      initialView: 'dayGridMonth'
-    });
-    calendar.render();
-  } else {
-    console.error('Calendar element not found');
-  }
-});
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    events: [
+      { start: '2024-04-01', imageUrl: 'image/face_img1.png' },
+      { start: '2024-04-02', imageUrl: 'image/face_img2.png' },
+      { start: '2024-04-03', imageUrl: 'image/face_img3.png' },
+      { start: '2024-04-04', imageUrl: 'image/face_img4.png' },
+      { start: '2024-04-05', imageUrl: 'image/face_img5.png' },
+      // ... その他のイベント
+    ],
+    eventContent: function(arg) {
+        var element = document.createElement('div');
+        var imageUrl = arg.event.extendedProps.imageUrl;
+        if (imageUrl) {
+          // `class="fc-event-image"` を img タグに適用します
+          element.innerHTML = `<img src="${imageUrl}" class="fc-event-image" />`;
+        } else {
+          element.innerText = arg.event.title;
+        }
+        return { domNodes: [element] };
+      }
+  });
 
+  calendar.render();
+});
